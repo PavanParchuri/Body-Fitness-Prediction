ea60a7a8c2d2b25196df2722bebcbfd6567c26d9
Pavan Parchuri <pavanparchuri1710@gmail.com>
2022-04-30 22:43:31 +0530

    Updated UI and improved backend

--- Code/Flask Files/model.py
+++ Code/Flask Files/model.py
@@ -10,22 +10,29 @@ le=LabelEncoder()
 df['bool_of_active']=le.fit_transform(df['bool_of_active'])
 df['mood']=le.fit_transform(df['mood'])
 
-#Independent variables
+# Independent variables
 x=df.iloc[:,[1,2,3,4,6]]
-#Dependent Variable
+x=x.to_numpy()
+# Dependent Variable
 y=df.iloc[:,5]
 
+# from sklearn.preprocessing import OneHotEncoder
+# oh=OneHotEncoder(categorical_features=[1])
+# x=oh.fit_transform(x).toarray()
 from sklearn.preprocessing import OneHotEncoder
-oh=OneHotEncoder(categorical_features=[1])
-x=oh.fit_transform(x).toarray()
+oh=OneHotEncoder()
+temp=oh.fit_transform(x[:,1:2]).toarray()
+#print(x,temp)
+x=np.delete(x,1,axis=1)
+x=np.concatenate((temp,x),axis=1) 
 x=x[:,1:]
 
-#Splitting the dataset into Train set and Test set
-from sklearn import model_selection, neighbors
+# Splitting the dataset into Train set and Test set
+# from sklearn import model_selection, neighbors
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.1,random_state=0)
 
-#Apply normalization to rescale the features to a standard range of values.
+# Apply normalization to rescale the features to a standard range of values.
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
@@ -34,6 +41,9 @@ x_test = scaler.transform(x_test)
 from sklearn.ensemble import GradientBoostingClassifier
 gb = GradientBoostingClassifier(max_depth=3)
 gb.fit(x_train,y_train)
+
+print("Train Accuracy:", gb.score(x_train,y_train)*100)
+print("Test Accuracy:", gb.score(x_test,y_test)*100)
 
 # Saving model to disk
 pickle.dump(gb, open('gbmodel.pkl','wb'))
